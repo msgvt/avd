23655802e1a3d1225334c77cc2fe937273b3fbf8
msgvt <157174267+msgvt@users.noreply.github.com>
2024-01-19 22:42:23 +0100

    Add files via upload

---
+++ avdcoprep.ps1
@@ -0,0 +1,34 @@
+﻿
+
+###########################################
+#    Execute Command In SYSTEM Context    #
+###########################################
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
+#cmd.exe /c "cmdkey.exe /add:$fileServer /user:$($user) /pass:$($secret)"
+
+
+################
+#    Profile   #
+################
+New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
+New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -ErrorAction Ignore
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value "\\sa100cloudavd.file.core.windows.net\profiles\profiles" -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ConcurrentUserSessions" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "IsDynamic" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ProfileType" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 30000 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "AccessNetworkAsComputerObject" -Value 1 -force
+
+
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-Command cmd.exe /c 'cmdkey /add:sa100cloudavd.file.core.windows.net /user:localhost\sa100cloudavd /pass:RTO/xI7ZGJSzXafFmdA1TaFCGTmoLZK6L8J3MF/l15fnfkz2N2PaUTYRYztGKjHukS22v2RrufSb+AStQJq0Iw=='" -WorkingDirectory 'C:\'
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
+Register-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -TaskName "CreateFolder2fa" -Description "Task to create a folder named 'test' on the desktop" -TaskPath "\"
+
+
+write-host "Configuration Complete"
